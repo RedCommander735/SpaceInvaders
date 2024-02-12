3140c8cd7215ce3c0937544163bfcf8f947921e1
TheRedKeks <40245965+RedCommander735@users.noreply.github.com>
2024-02-12 23:34:55 +0100

    Bullets, Tank movements

--- src/space_invaders.rs
+++ src/space_invaders.rs
@@ -1,28 +1,30 @@
+use std::time::Duration;
+
+use bevy::app::AppExit;
 use bevy::prelude::*;
 use bevy::render::extract_component::ExtractComponent;
 use bevy::sprite::collide_aabb::{collide, Collision};
 use bevy::sprite::{MaterialMesh2dBundle, Mesh2d};
+use bevy::time::Stopwatch;
 
 pub const WINDOW_SIZE: Vec2 = Vec2::new(900., 600.);
 // delta x: 450 | delta y: 300
 
-pub const ALIEN_TOP: f32 = 250.;
-pub const ALIEN_BOTTOM: f32 = 0.;
-pub const ALIEN_LEFT: f32 = -200.;
-pub const ALIEN_RIGHT: f32 = 200.;
-
 pub const PADDING_ALIEN: f32 = 4.;
 pub const ALIEN_SIZE: Vec2 = Vec2::new(28., 20.);
 const TANK_SIZE: Vec2 = Vec2::new(24., 28.);
+const BULLET_SIZE: Vec2 = Vec2::new(4., 12.);
 
 const VERT_STEP: f32 = ALIEN_SIZE.y + 2. * PADDING_ALIEN;
 
 const ALIEN_SPEED: f32 = 10.;
 const TANK_SPEED: f32 = 50.;
+const BULLET_SPEED: f32 = 100.;
 const VERT_STEP_DISTANCE_FROM_MIDDLE: u32 = 50;
 
+const BULLET_LIMIT: i16 = 5;
+
 pub const DEATH_LINE: f32 = -100.;
-pub const TOP_END: f32 = 300.;
 
 pub struct SpaceInvaders;
 
@@ -31,7 +33,15 @@ impl Plugin for SpaceInvaders {
         app.add_systems(Startup, setup)
             .add_systems(
                 FixedUpdate,
-                (move_aliens, move_tank, check_for_collisions).chain(),
+                (
+                    move_aliens,
+                    move_tank,
+                    fire_bullet,
+                    bullet_system,
+                    check_for_collisions,
+                    check_win,
+                )
+                    .chain(),
             )
             .add_event::<CollisionEvent>();
     }
@@ -70,19 +80,33 @@ pub struct Velocity(Vec2);
 #[derive(Component)]
 pub struct Alive(bool);
 
+#[derive(Resource)]
+struct TimeSinceLastShot {
+    time: Stopwatch,
+}
+
 #[derive(Component)]
 struct Movement {
     speed: f32,
-    direction: Direction,
+    direction_x: DirectionX,
+    direction_y: DirectionY,
 }
 
 #[derive(Component)]
 struct HorizontalStep(f32);
 
 #[derive(Clone, Copy)]
-enum Direction {
+enum DirectionX {
+    NONE = 0,
     LEFT = -1,
     RIGHT = 1,
+}
+
+#[derive(Clone, Copy)]
+enum DirectionY {
+    NONE = 0,
+    DOWN = -1,
+    UP = 1,
 }
 
 fn setup(
@@ -91,8 +115,6 @@ fn setup(
     mut materials: ResMut<Assets<ColorMaterial>>,
     asset_server: Res<AssetServer>,
 ) {
-    cmd.spawn(());
-
     cmd.spawn((
         ColorMesh2dBundle {
             mesh: meshes.add(Mesh::from(shape::Quad::default())).into(),
@@ -111,7 +133,7 @@ fn setup(
 
     spawn_aliens(&mut cmd, 4, 7, &asset_server);
 
-    // Spawn Connon
+    // Spawn Tank
     cmd.spawn((
         SpriteBundle {
             texture: asset_server.load("tank2x.png"),
@@ -123,18 +145,22 @@ fn setup(
         Alive(true),
         Lives(3),
     ));
+
+    cmd.insert_resource(TimeSinceLastShot {
+        time: Stopwatch::new(),
+    })
 }
 
 fn check_for_collisions(
-    mut commands: Commands,
+    mut cmd: Commands,
     // mut scoreboard: ResMut<Scoreboard>,
-    mut alien_query: Query<&Transform, With<Alien>>,
-    collider_query: Query<(Entity, &Transform, Option<&DeathLine>), With<Collider>>,
+    mut alien_query: Query<(Entity, &Transform), With<Alien>>,
+    death_line_query: Query<(Entity, &Transform), (With<Collider>, With<DeathLine>)>,
+    bullet_query: Query<(Entity, &Transform), (With<Collider>, With<Bullet>)>,
     mut collision_events: EventWriter<CollisionEvent>,
 ) {
-    // check collision with walls
-    for (collider_entity, transform, maybe_death_line) in &collider_query {
-        for alien in &mut alien_query {
+    for (collider_entity, transform) in &death_line_query {
+        for (_entity, alien) in &mut alien_query {
             let collision = collide(
                 alien.translation,
                 ALIEN_SIZE,
@@ -146,10 +172,25 @@ fn check_for_collisions(
                 collision_events.send_default();
 
                 // Bricks should be despawned and increment the scoreboard on collision
-                if maybe_death_line.is_some() {
-                    // TODO - Implement life loss and round restart here
-                    todo!()
-                }
+                // TODO - Implement life loss and round restart here
+                todo!()
+            }
+        }
+    }
+
+    for (collider_entity, transform) in &bullet_query {
+        for (entity, alien) in &mut alien_query {
+            let collision = collide(
+                alien.translation,
+                ALIEN_SIZE,
+                transform.translation,
+                BULLET_SIZE,
+            );
+            if let Some(collision) = collision {
+                // Sends a collision event so that other systems can react to the collision
+                collision_events.send_default();
+                cmd.entity(collider_entity).despawn();
+                cmd.entity(entity).despawn();
             }
         }
     }
@@ -165,12 +206,8 @@ fn spawn_aliens(
     // Add variable to change height depending on level
     let alien_bottom_edge = DEATH_LINE + 100.;
 
-    // Because we need to round the number of columns,
-    // the space on the top and sides of the bricks only captures a lower bound, not an exact value
-    let center_of_aliens = 0.;
-    let left_edge_of_aliens = center_of_aliens
-        - (n_columns as f32 / 2.0 * (ALIEN_SIZE.x + PADDING_ALIEN * 2.))
-        + PADDING_ALIEN;
+    let left_edge_of_aliens =
+        -(n_columns as f32 / 2.0 * (ALIEN_SIZE.x + PADDING_ALIEN * 2.)) + PADDING_ALIEN;
 
     // In Bevy, the `translation` of an entity describes the center point,
     // not its bottom-left corner
@@ -198,7 +235,8 @@ fn spawn_aliens(
                 Alive(true),
                 Movement {
                     speed: ALIEN_SPEED,
-                    direction: Direction::RIGHT,
+                    direction_x: DirectionX::RIGHT,
+                    direction_y: DirectionY::NONE,
                 },
                 HorizontalStep(0.),
             ));
@@ -212,16 +250,17 @@ fn move_aliens(
     mut query: Query<(&mut Movement, &mut HorizontalStep, &mut Transform), With<Alien>>,
 ) {
     for (mut movement, mut h_step, mut transform) in &mut query {
-        let delta = movement.speed * movement.direction as i8 as f32 * time.delta_seconds();
+        let delta = movement.speed * movement.direction_x as i8 as f32 * time.delta_seconds();
         transform.translation.x += delta;
         h_step.0 += delta;
         // eprintln!("{}", h_step.0);
 
         if h_step.0.abs() >= VERT_STEP_DISTANCE_FROM_MIDDLE as f32 {
             transform.translation.y -= VERT_STEP;
-            movement.direction = match movement.direction {
-                Direction::LEFT => Direction::RIGHT,
-                Direction::RIGHT => Direction::LEFT,
+            movement.direction_x = match movement.direction_x {
+                DirectionX::LEFT => DirectionX::RIGHT,
+                DirectionX::RIGHT => DirectionX::LEFT,
+                DirectionX::NONE => DirectionX::NONE,
             }
         }
     }
@@ -243,15 +282,76 @@ fn move_tank(
         direction += 1.0;
     }
 
-    // Calculate the new horizontal paddle position based on player input
     let new_tank_position =
         tank_transform.translation.x + direction * TANK_SPEED * time.delta_seconds();
-
-    // Update the paddle position,
-    // making sure it doesn't cause the paddle to leave the arena
 
     let left_bound = -WINDOW_SIZE.x / 2.0 + TANK_SIZE.x / 2.0;
     let right_bound = WINDOW_SIZE.x / 2.0 - TANK_SIZE.x / 2.0;
 
     tank_transform.translation.x = new_tank_position.clamp(left_bound, right_bound);
+}
+
+fn fire_bullet(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut cmd: Commands,
+    query: Query<&Transform, With<Tank>>,
+    bullets: Query<&Transform, With<Bullet>>,
+    asset_server: Res<AssetServer>,
+    mut stopwatch: ResMut<TimeSinceLastShot>,
+    time: Res<Time>,
+) {
+    let tank_transform = query.single();
+
+    stopwatch.time.tick(time.delta());
+
+    // eprintln!("{:?}", stopwatch.0);
+
+    let bullet_limit = BULLET_LIMIT - bullets.iter().count() as i16;
+
+    if keyboard_input.just_pressed(KeyCode::Space)
+        && bullet_limit > 0
+        && stopwatch.time.elapsed_secs() > 0.5
+    {
+        stopwatch.time.reset();
+        cmd.spawn((
+            SpriteBundle {
+                texture: asset_server.load("bullet2x.png"),
+                transform: Transform {
+                    translation: tank_transform.translation,
+                    ..Default::default()
+                },
+                ..default()
+            },
+            Bullet,
+            Collider,
+            Alive(true),
+            Movement {
+                speed: BULLET_SPEED,
+                direction_x: DirectionX::NONE,
+                direction_y: DirectionY::UP,
+            },
+            HorizontalStep(0.),
+        ));
+    }
+}
+
+fn bullet_system(
+    mut bullets: Query<(&Movement, &mut Transform, Entity), With<Bullet>>,
+    time: Res<Time>,
+    mut cmd: Commands,
+) {
+    for (movement, mut transform, entity) in &mut bullets {
+        let delta = movement.speed * movement.direction_y as i8 as f32 * time.delta_seconds();
+        transform.translation.y += delta;
+
+        if transform.translation.y + BULLET_SIZE.y / 2. >= WINDOW_SIZE.y / 2. {
+            cmd.entity(entity).despawn();
+        }
+    }
+}
+
+fn check_win(aliens: Query<Entity, With<Alien>>, mut exit: EventWriter<AppExit>) {
+    if aliens.iter().count() == 0 {
+        exit.send(AppExit)
+    }
 }
